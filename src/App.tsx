76cc8fad3a41ad1a1990e548bfc7c9ea4bd70a56
RickyBv1 <rickycortes668668@gmail.com>
2025-05-23 23:19:07 -0400

    feat: implement sorting and filtering functionality in user list

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,112 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import { UsersList } from "./components/UsersList";
-import type { User } from "./types.d";
+import { SortBy, type User } from "./types.d";
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [showColors, setShowColors] = useState(false);
-  const [sortByCountry, setSortByCountry] = useState(false);
+  const [sorting, setSorting] = useState<SortBy>(SortBy.NONE);
+  const [filterCountry, setFilterCountry] = useState<string | null>(null);
+
+  const originalUsers = useRef<User[]>([]);
+  // useRef -> para guardar un valor
+  // que queremos que se comparta entre renderizados
+  // pero que al cambiar, no vuelva a renderizar el componente
 
   const toggleColors = () => {
     setShowColors(!showColors);
   };
 
   const toggleSortByCountry = () => {
-    setSortByCountry(!sortByCountry);
+    const newSortingValue =
+      sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE;
+    setSorting(newSortingValue);
   };
 
-  const sortedUsers = sortByCountry
-    ? users.toSorted((a, b) => {
-        return a.location.country.localeCompare(b.location.country);
-      })
-    : users;
+  const handleReset = () => {
+    setUsers(originalUsers.current);
+  };
+
+  const handleDelete = (email: string) => {
+    const filteredUsers = users.filter((user) => user.email !== email);
+    setUsers(filteredUsers);
+  };
+
+  const handleChangeSort = (sort: SortBy) => {
+    setSorting(sort);
+  };
 
   useEffect(() => {
     fetch("https://randomuser.me/api?results=100")
       .then(async (res) => await res.json())
       .then((res) => {
         setUsers(res.results);
+        originalUsers.current = res.results;
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
 
+  const filteredUsers = useMemo(() => {
+    console.log("calculate filteredUsers");
+    return filterCountry != null && filterCountry.length > 0
+      ? users.filter((user) => {
+          return user.location.country
+            .toLowerCase()
+            .includes(filterCountry.toLowerCase());
+        })
+      : users;
+  }, [users, filterCountry]);
+
+  const sortedUsers = useMemo(() => {
+    console.log("calculate sortedUsers");
+
+    if (sorting === SortBy.NONE) return filteredUsers;
+
+    const compareProperties: Record<string, (user: User) => any> = {
+      [SortBy.COUNTRY]: (user) => user.location.country,
+      [SortBy.NAME]: (user) => user.name.first,
+      [SortBy.LAST]: (user) => user.name.last,
+    };
+
+    return filteredUsers.toSorted((a, b) => {
+      const extractProperty = compareProperties[sorting];
+      return extractProperty(a).localeCompare(extractProperty(b));
+    });
+  }, [filteredUsers, sorting]);
+
   return (
-    <>
+    <div className="App">
       <h1>Technical Test</h1>
       <header>
         <button onClick={toggleColors}>Color rows</button>
-        <button onClick={toggleSortByCountry} style={{ marginLeft: "10px" }}>
-          {sortByCountry ? "Stop sorting by country" : "Sort by country"}
+
+        <button onClick={toggleSortByCountry}>
+          {sorting === SortBy.COUNTRY
+            ? "Stop sorting by country"
+            : "Sort by country"}
         </button>
+
+        <button onClick={handleReset}>Undo delete</button>
+
+        <input
+          placeholder="Filter by country"
+          onChange={(e) => {
+            setFilterCountry(e.target.value);
+          }}
+        />
       </header>
       <main>
-        <UsersList showColors={showColors} users={sortedUsers} />
+        <UsersList
+          changeSorting={handleChangeSort}
+          deleteUser={handleDelete}
+          showColors={showColors}
+          users={sortedUsers}
+        />
       </main>
-    </>
+    </div>
   );
 }
 
