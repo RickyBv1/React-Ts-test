906a48c3a666cd95c99457a8119489257c098b19
RickyBv1 <rickycortes668668@gmail.com>
2025-05-26 21:48:33 -0400

    feat: add loading and error handling in user fetching process

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
   const [sorting, setSorting] = useState<SortBy>(SortBy.NONE);
   const [filterCountry, setFilterCountry] = useState<string | null>(null);
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const originalUsers = useRef<User[]>([]);
 
   const toggleColors = () => {
@@ -35,14 +38,24 @@ function App() {
   };
 
   useEffect(() => {
-    fetch("https://randomuser.me/api?results=100")
-      .then(async (res) => await res.json())
+    setLoading(true);
+    setError(false);
+
+    fetch("https://randomuser.me/api?results=10")
+      .then(async (res) => {
+        if (!res.ok) throw new Error("Failed to fetch users");
+        return await res.json();
+      })
       .then((res) => {
         setUsers(res.results);
         originalUsers.current = res.results;
       })
       .catch((err) => {
+        setError(err);
         console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
@@ -93,12 +106,20 @@ function App() {
         />
       </header>
       <main>
-        <UsersList
-          changeSorting={handleChangeSort}
-          deleteUser={handleDelete}
-          showColors={showColors}
-          users={sortedUsers}
-        />
+        {loading && <strong>Loading...</strong>}
+
+        {!loading && error && <p>Error loading users</p>}
+
+        {!loading && !error && users.length === 0 && <p>No users found</p>}
+
+        {!loading && !error && users.length > 0 && (
+          <UsersList
+            changeSorting={handleChangeSort}
+            deleteUser={handleDelete}
+            showColors={showColors}
+            users={sortedUsers}
+          />
+        )}
       </main>
     </div>
   );
